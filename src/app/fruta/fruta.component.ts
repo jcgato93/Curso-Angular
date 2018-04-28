0b38083d1259df904d8bfd660509f776225b57a4
juan camilo castillo saucedo <jcgato93@hotmail.com>
2018-04-28 16:53:14 -0500

    Diferencia entre var y let

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -31,14 +31,27 @@ export class FrutaComponent{
     //luego de del constructor 
     ngOnInit()
     {
-    //llamar metodo
-    this.holaMundo(this.nombre);
+        //llamar metodo
+        this.holaMundo(this.nombre);
+
+        //Variables y alcance 
+        var uno = 8;
+        var dos = 15;
+
+        if (uno === 8) {
+            let uno = 3;//lef sirve para declarar una variable defienida dentro de un bloque de codigo
+            var dos = 88;
+            console.log("dentro del if : "+uno);
+        }
+        console.log("fuera del if :"+uno);
     }
 
 
+    //#region  metodos
     //Metodo
     holaMundo(nombre:String)
     {
         alert('Hola mundo '+nombre);        
     }
+    //#endregion
 }
